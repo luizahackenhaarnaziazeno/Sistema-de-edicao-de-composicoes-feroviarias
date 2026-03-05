bb01813a90863fbcbefceaf8f774864e87cd759a
Luiza Hackenhaar Naziazeno <142232479+luizahackenhaarnaziazeno@users.noreply.github.com>
2026-03-05 15:01:33 -0300

    remove comments

--- Garagem De Trens/GaragemVagoes.java
+++ Garagem De Trens/GaragemVagoes.java
@@ -1,87 +1,85 @@
 import java.util.ArrayList;
 
 public class GaragemVagoes {
-private ArrayList<Vagoes>vagoes;// Declaração do atributo para armazenar vagões como uma lista de objetos vagões.
-private int proxLivre;// Variável para acompanhar a próxima posição livre na lista de vagões
-private ArrayList<GaragemTrem>garagemtrem;//Declaração do atributo para armazenar garargemtrem como uma lista de objetos garagemtrem.
-private ArrayList<GaragemLocomotiva>garagemlocomotiva;// Declaração do atributo para armazenar garagemlocomotiva como uma lista de objetos garagemlocomotiva.
+private ArrayList<Vagoes>vagoes;
+private int proxLivre;
+private ArrayList<GaragemTrem>garagemtrem;
+private ArrayList<GaragemLocomotiva>garagemlocomotiva;
 
 public GaragemVagoes(ArrayList<Vagoes> vagoes, int proxLivre) {
     this.vagoes = vagoes;
     this.proxLivre = proxLivre;
 }
 
-// Método para estacionar uma vagões na garagem
+
 public void estacionaVagoes(Vagoes v){
 for(GaragemLocomotiva gl:garagemlocomotiva){
 if(gl.estacionaLocomotiva(null)){
     System.out.println("O Vagão não pode ser inserido,pois não há uma locomotiva");
     }else{
-vagoes.add(v);// Adiciona a vagões à lista de locomotivas
-proxLivre++; // Incrementa o contador de próxima posição livre
+vagoes.add(v);
+proxLivre++; 
 }
 }
 }
 
-public void setProxLivre(int proxLivre) {// Setter para a próxima posição livre
+public void setProxLivre(int proxLivre) {
     this.proxLivre = proxLivre;
 }
 
 
-public Vagoes setIdVagoes(int id){// Método para procurar um vagão pelo seu ID
-for(Vagoes v:vagoes){// Itera sobre a lista de vagões
-if(v.getIdvagao()==id){// Verifica se o ID do vagão atual é igual ao ID fornecido
-return v; // Se encontrar uma correspondência, retorna o vagão  
+public Vagoes setIdVagoes(int id){
+for(Vagoes v:vagoes){
+if(v.getIdvagao()==id){
+return v; 
 }    
 }    
-return (null);// Se não encontrar correspondência, retorna null
+return (null);
 }
 
-public ArrayList<Vagoes>gettoVagoes(){// Método para obter a lista de vagões
+public ArrayList<Vagoes>gettoVagoes(){
 return vagoes;    
 }
 
 public Vagoes getIdvagao(){
-    for(Vagoes v:vagoes){//Para cada vagão na lista de vagões.
-     v.getIdvagao();//Chama o método getIdvagao() para cada vagão na lista.    
+    for(Vagoes v:vagoes){
+     v.getIdvagao();   
         }
-    return null;//Retorna null
+    return null;
     
 }
 
 
-public Vagoes RetiraVagoes(int id){// Método para retirar um vagão da garagem pelo seu ID
-for(int i=0;i<vagoes.size();i++){// Itera sobre a lista de vagões
-if(vagoes.get(i).getIdvagao()==id){;// Verifica se o ID do vagão atual é igual ao ID fornecido
-return vagoes.remove(i);// Se encontrar uma correspondência, remove o vagão da lista
+public Vagoes RetiraVagoes(int id){
+for(int i=0;i<vagoes.size();i++){
+if(vagoes.get(i).getIdvagao()==id){;
+return vagoes.remove(i);
 }    
 }
-// Se não encontrar correspondência, retorna null
+
 return null;
 }
 
-// Método para listar os vagões não utilizados
+
 public void listarVagoesLivres() {
     System.out.println("Vagões não utilizados na composição de nenhum trem:");
     for (Vagoes vagoes : vagoes) {
-        // Verifica se o vagão não foi utilizado na composição de nenhum trem
+        
         if (!foiUtilizado(vagoes)) {
             System.out.println(vagoes.toString());
         }
     }
 }
 
-// Método privado para verificar se um vagão foi utilizado na composição de algum trem
+
 private boolean foiUtilizado(Vagoes vagoes) {
-    // Itera sobre os trens na garagem de trens
     for (GaragemTrem gt : garagemtrem) {
         gt.listarTrens();
-        // Verifica se o vagão está presente na composição do trem
         if (gt.listarTrens()==gt.getIdTrem()) {
-            return true; // Retorna true se o vagão foi utilizado em algum trem
+            return true; 
         }
     }
-    return false; // Retorna false se o vagão não foi utilizado em nenhum trem
+    return false; 
 }
 
 
@@ -93,6 +91,7 @@ public String toString() {
 
 
 }
+
 
 
 
